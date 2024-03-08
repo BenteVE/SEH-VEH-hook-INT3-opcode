3043128d9c25c6ede5aa0b620ccea60ede458df1
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-03-08 18:08:02 +0100

    Test using hook function with trampoline (doesn't work)

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,73 +1,54 @@
 #include <Windows.h>
-#include <TlHelp32.h>
-#include <stdio.h>
 #include "Console.h"
 
 Console console;
 
 // This stub function contains the first instruction of the function with the breakpoint
-// => then the function jumps to the second instruction of the original function 
+// => then the function jumps to the second instruction of the original function
 // => this way we execute every instruction of the original function without triggering another exception and an infinite loop
 DWORD func_addr = NULL;
 DWORD func_addr_offset = NULL;
-void __declspec(naked) MessageBoxW_trampoline(void) {
+int WINAPI MessageBoxW_trampoline(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType) {
+	fprintf(console.stream, "Inside trampoline, before assembly\n");
 	__asm {
 		mov edi, edi
 		jmp[func_addr_offset]
 	}
+	fprintf(console.stream, "After executing assembly\n");
 	// Note: To find the first instruction of the hook function, you can use documentation (if available) or Ghidra
 	// => Make sure you analyze the correct DLL (32-bit in SysWOW64 folder or 64-bit in System32 folder).
 	// Note: it should also be possible to dynamically copy the instructions at func_addr instead of hardcoding it in assembly here
 }
 
-// print the content of the registers and the stack to the console 
-void print_parameters(PCONTEXT debug_context) {
-	fprintf(console.stream, "Registers:\n");
-	fprintf(console.stream, "EAX: %X EBX: %X\n", debug_context->Eax, debug_context->Ebx);
-	fprintf(console.stream, "ECX: %X EDX: %X\n", debug_context->Ecx, debug_context->Edx);
-	fprintf(console.stream, "ESP: %X EBP: %X\n", debug_context->Esp, debug_context->Ebp);
-	fprintf(console.stream, "ESI: %X EDI: %X\n", debug_context->Esi, debug_context->Edi);
+int WINAPI hookedMessageBox(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType)
+{
+	fprintf(console.stream, "Executing hooked function, calling trampoline\n");
 
-	// ESP is the stack pointer, all parameters are on the stack
-	// To find the parameters, you can use documentation (if available) or a decompiler like Ghidra.
-	fprintf(console.stream, "Function parameters:\n");
-	fprintf(console.stream, "HWND: %p\n", (HWND)(*(PDWORD)(debug_context->Esp + 0x4)));
+	// Execute the true function from the hook with changed arguments
+	// The true function still has the changed byte (0xCC) at its start
+	// - we can temporarily change the byte back to the original byte (not implemented here)
+	// - we can use a trampoline to avoid the changed byte
+	LPCTSTR lpCaptionChanged = L"Hooked MessageBox";
+	int retval = MessageBoxW_trampoline(hWnd, lpText, lpCaptionChanged, uType);
 
-	// MessageBoxW uses wide strings
-	fwprintf(console.stream, L"lptext:    %s\n", (LPCWSTR)(*(PDWORD)(debug_context->Esp + 0x8)));
-	fwprintf(console.stream, L"lpcaption: %s\n", (LPCWSTR)(*(PDWORD)(debug_context->Esp + 0xC)));
+	fprintf(console.stream, "After executing true function\n");
 
-	fprintf(console.stream, "type: % X\n", (UINT)(*(PDWORD)(debug_context->Esp + 0x10)));
-	// Example: 23h == MB_ICONQUESTION + MB_YESNOCANCEL
+	return retval;
 }
-
-// Change the stack to update the caption shown in the MessageBox
-LPCWSTR hook_caption = L"Hooked MessageBox";
-void modify_stack(PCONTEXT debug_context) {
-	DWORD oldProtection{};
-	VirtualProtect((LPVOID)(debug_context->Esp + 0xC), sizeof(PDWORD), PAGE_READWRITE, &oldProtection);
-	*(PDWORD)(debug_context->Esp + 0xC) = (DWORD)hook_caption;
-	VirtualProtect((LPVOID)(debug_context->Esp + 0xC), sizeof(PDWORD), oldProtection, &oldProtection);
-}
-
 
 // Used to pass to the Structured/Vectored Exception Handler
 LONG WINAPI ExceptionFilter(PEXCEPTION_POINTERS ExceptionInfo) {
-	// Check if the ExceptionFilter caught the 0xCC opcode exception or another unrelated exception
-	// The 0xCC opcode causes an EXCEPTION_BREAKPOINT 
+	// Check if the ExceptionFilter caught the exception of the INT3 opcode or another unrelated exception
 	if (ExceptionInfo->ExceptionRecord->ExceptionCode == EXCEPTION_BREAKPOINT) {
 		// Check if the address of the exception matches the address of the hooked function
 		if ((DWORD)ExceptionInfo->ExceptionRecord->ExceptionAddress == func_addr) {
+			fprintf(console.stream, "Breakpoint hit, reading registers and function parameters ...\n");
+
 			// Use the ContextRecord to view/modify the arguments of the hooked function
 			PCONTEXT debug_context = ExceptionInfo->ContextRecord;
-			fprintf(console.stream, "Breakpoint hit, reading registers and function parameters ...\n");
-			print_parameters(debug_context);
 
-			fprintf(console.stream, "Modifying parameters on stack\n");
-			modify_stack(debug_context);
-
-			fprintf(console.stream, "Using trampoling\n");
-			debug_context->Eip = (DWORD)&MessageBoxW_trampoline;
+			// deviate the execution of the program to the hook function
+			debug_context->Eip = (DWORD)hookedMessageBox;
 
 			return EXCEPTION_CONTINUE_EXECUTION;
 		}
@@ -85,12 +66,14 @@ DWORD WINAPI testHook(PVOID base) {
 	return 0;
 }
 
-CHAR overwrite_address(PCHAR address, CHAR new_opcode) {
+// Replace the opcode
+CHAR original_opcode = 0;
+CHAR overwrite_opcode(PCHAR address, CHAR new_opcode) {
 	// Change the protection so we can overwrite the pointer, store the old protection
 	DWORD old_protection{};
 	VirtualProtect(address, 1, PAGE_READWRITE, &old_protection);
 
-	// Overwrite the address with the given opcode, store the old opcode to return
+	// Overwrite the address with a pointer to another function
 	CHAR old_opcode = *address;
 	*address = new_opcode;
 
@@ -103,9 +86,6 @@ CHAR overwrite_address(PCHAR address, CHAR new_opcode) {
 // When using the Vectored Exception Handler, this is used to remove the VEH
 // (unused when using SEH instead) 
 PVOID VEH_Handle = nullptr;
-
-// Store the original opcode to restore when unhooking
-CHAR original_opcode = 0;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
@@ -132,16 +112,16 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		func_addr = (DWORD)GetProcAddress(h_module, function_name);
 		func_addr_offset = func_addr + 0x2; // jump over first instruction (instruction 'mov edi, edi' is 2 bytes long => view in Ghidra)
 
-		// Replace the address with 0xCC opcode
-		original_opcode = overwrite_address((PCHAR)func_addr, (CHAR)0xCC);
+		// replace the opcode at the start of the true function
+		original_opcode = overwrite_opcode((PCHAR)func_addr, 0xCC);
 
 		fprintf(console.stream, "Setting Exception Filter.\n");
 		// Set the Exception Handler (choose SEH or VEH!)
-		SetUnhandledExceptionFilter(ExceptionFilter);
-		//VEH_Handle = AddVectoredExceptionHandler(1, ExceptionFilter);
+		//SetUnhandledExceptionFilter(ExceptionFilter);
+		VEH_Handle = AddVectoredExceptionHandler(1, ExceptionFilter);
 
 		// Test: also set for this thread and call function
-		CreateThread(nullptr, NULL, testHook, hModule, NULL, nullptr);
+		//CreateThread(nullptr, NULL, testHook, hModule, NULL, nullptr);
 
 	}
 
@@ -151,11 +131,10 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		fprintf(console.stream, "Uninstalling the hook ...\n");
 
 		// Remove the Exception Handler (choose SEH or VEH!)
-		SetUnhandledExceptionFilter(NULL);
-		//RemoveVectoredExceptionHandler(VEH_Handle);
+		//SetUnhandledExceptionFilter(NULL);
+		RemoveVectoredExceptionHandler(VEH_Handle);
 
-		// Replace the address with 0xCC opcode
-		overwrite_address((PCHAR)func_addr, original_opcode);
+		overwrite_opcode((PCHAR)func_addr, original_opcode);
 
 		// Open a MessageBox to allow reading the output
 		MessageBoxW(NULL, L"Press Ok to close", L"Closing", NULL);
@@ -163,4 +142,5 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 	}
 	return TRUE;
 }
+
 
